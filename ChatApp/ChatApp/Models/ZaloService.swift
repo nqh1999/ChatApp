40a5e9eaf974e578a14d5886061afdafb5bc0e1e
Nguyen Quang Huy <ptitnqh@gmail.com>
2022-12-23 17:07:41 +0700

    no message

--- ChatApp/ChatApp/Models/ZaloService.swift
+++ ChatApp/ChatApp/Models/ZaloService.swift
@@ -33,30 +33,24 @@ class ZaloService {
     private func onAuthenticateComplete(with response: ZOOauthResponseObject?, completed: @escaping (String, String, String) -> Void) {
         guard let response = response, response.errorCode != -1001 else { return }
         if response.isSucess {
-            self.getProfile(response.oauthCode) { name, id, url in
-                completed(name, id, url)
+            ZaloSDK.sharedInstance().getAccessToken(withOAuthCode: response.oauthCode, codeVerifier: self.codeVerifier) { tokenResponse in
+                guard let accessToken = tokenResponse?.accessToken else { return }
+                ZaloSDK.sharedInstance().getZaloUserProfile(withAccessToken: accessToken) { profile in
+                    guard let profile = profile,
+                        profile.isSucess,
+                        let name = profile.data["name"] as? String,
+                        let id = profile.data["id"] as? String,
+                        let picture = profile.data["picture"] as? [String: Any?],
+                        let pictureData = picture["data"] as? [String: Any?],
+                        let url = pictureData["url"] as? String
+                        else {
+                        return
+                    }
+                    completed(name, id, url)
+                }
             }
         } else {
             return
-        }
-    }
-
-    private func getProfile(_ oauthCode: String?, completed: @escaping (String, String, String) -> Void) {
-        ZaloSDK.sharedInstance().getAccessToken(withOAuthCode: oauthCode, codeVerifier: self.codeVerifier) { (tokenResponse) in
-            guard let accessToken = tokenResponse?.accessToken else { return }
-            ZaloSDK.sharedInstance().getZaloUserProfile(withAccessToken: accessToken) { profile in
-                guard let profile = profile,
-                    profile.isSucess,
-                    let name = profile.data["name"] as? String,
-                    let id = profile.data["id"] as? String,
-                    let picture = profile.data["picture"] as? [String: Any?],
-                    let pictureData = picture["data"] as? [String: Any?],
-                    let url = pictureData["url"] as? String
-                    else {
-                    return
-                }
-                completed(name, id, url)
-            }
         }
     }
 }
