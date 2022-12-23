89b955b122572aed730bdea93522f1a99b3ce6ca
Nguyen Quang Huy <ptitnqh@gmail.com>
2022-12-23 17:21:30 +0700

    clean code

--- ChatApp/ChatApp/Models/ZaloService.swift
+++ ChatApp/ChatApp/Models/ZaloService.swift
@@ -19,38 +19,31 @@ class ZaloService {
         self.codeVerifier = generateCodeVerifier() ?? ""
         self.codeChallenage = generateCodeChallenge(codeVerifier: self.codeVerifier) ?? ""
         ZaloSDK.sharedInstance().authenticateZalo(with: ZAZaloSDKAuthenTypeViaWebViewOnly, parentController: vc, codeChallenge: self.codeChallenage, extInfo: Constant.EXT_INFO) { [weak self] response in
-            
-            self?.onAuthenticateComplete(with: response) { name, id, url in
-                completed(name, id, url)
+            guard let response = response, response.errorCode != -1001 else { return }
+            if response.isSucess {
+                ZaloSDK.sharedInstance().getAccessToken(withOAuthCode: response.oauthCode, codeVerifier: self?.codeVerifier) { tokenResponse in
+                    guard let accessToken = tokenResponse?.accessToken else { return }
+                    ZaloSDK.sharedInstance().getZaloUserProfile(withAccessToken: accessToken) { profile in
+                        guard let profile = profile,
+                            profile.isSucess,
+                            let name = profile.data["name"] as? String,
+                            let id = profile.data["id"] as? String,
+                            let picture = profile.data["picture"] as? [String: Any?],
+                            let pictureData = picture["data"] as? [String: Any?],
+                            let url = pictureData["url"] as? String
+                            else {
+                            return
+                        }
+                        completed(name, id, url)
+                    }
+                }
+            } else {
+                return
             }
         }
     }
     
     func logout() {
         ZaloSDK.sharedInstance().unauthenticate()
-    }
-    
-    private func onAuthenticateComplete(with response: ZOOauthResponseObject?, completed: @escaping (String, String, String) -> Void) {
-        guard let response = response, response.errorCode != -1001 else { return }
-        if response.isSucess {
-            ZaloSDK.sharedInstance().getAccessToken(withOAuthCode: response.oauthCode, codeVerifier: self.codeVerifier) { tokenResponse in
-                guard let accessToken = tokenResponse?.accessToken else { return }
-                ZaloSDK.sharedInstance().getZaloUserProfile(withAccessToken: accessToken) { profile in
-                    guard let profile = profile,
-                        profile.isSucess,
-                        let name = profile.data["name"] as? String,
-                        let id = profile.data["id"] as? String,
-                        let picture = profile.data["picture"] as? [String: Any?],
-                        let pictureData = picture["data"] as? [String: Any?],
-                        let url = pictureData["url"] as? String
-                        else {
-                        return
-                    }
-                    completed(name, id, url)
-                }
-            }
-        } else {
-            return
-        }
     }
 }
